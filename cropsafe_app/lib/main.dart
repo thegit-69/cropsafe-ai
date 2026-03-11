91e53df2b8f1590a25f5b9d8bc1fbf0a7ab81d7a
Dasarath C <robotzeror@gmail.com>
2026-03-11 09:05:48 +0530

    changes in main.dart

--- cropsafe_app/lib/main.dart
+++ cropsafe_app/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'firebase_options.dart';
